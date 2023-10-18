953a81d63c2fc6a5fe374772fb8a5f4e917c460d
Rahul Marban <rahulmarban@gmail.com>
2023-10-18 03:52:52 +0000

    Configurations

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
+  swcMinify: true,
+  reactStrictMode: true,
+  compress: true,
+  webpack: (config) => {
+    config.experiments = {
+      topLevelAwait: true,
+      layers: true,
+    };
+    return config;
+  },
   rewrites: async () => {
     return [
       {
