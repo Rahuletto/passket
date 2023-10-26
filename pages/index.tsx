41e75534652782ad16253c795323582efbc1d7a8
Rahul Marban <rahulmarban@gmail.com>
2023-10-26 15:48:38 +0000

    visible and hide password

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { useMemo, useState } from "react";
 import styles from "../styles/Pass.module.css"
 
 import Drop from "../components/Drop";
-import {Password} from '../utils/types'
+import { Password } from '../utils/types'
 
 const color = [
   "white",
@@ -18,12 +18,34 @@ export default function Home() {
   const [passes, setPasses] = useState<Password[]>([]);
 
   const userid = "12345678"
-  
+
   useMemo(() => {
     fetch(`/api/fetch/${userid}`).then(a => a.json()).then(res => {
       setPasses(res.keys)
     })
   }, [])
+
+  function visible(element, pass) {
+    const pin = prompt("PIN Code")
+    if (pin == "1234") {
+      fetch('/api/decrypt', {
+        method: "POST",
+        body: JSON.stringify({ input: pass })
+      }).then(data => data.json()).then(res => {
+        element.innerHtml = res.decrypted ? res.decrypted : pass
+        element.style.filter = "none";
+      }).catch(() => {
+        element.innerHtml = pass
+        element.style.filter = "none";
+      })
+
+      setTimeout(() => {
+        element.innerHtml = pass
+        element.style.filter = "blur(3px)";
+      }, 10 * 1000)
+      
+    }
+  }
 
   return (
     <main>
@@ -33,9 +55,9 @@ export default function Home() {
           passes && passes.map(pass => {
             return (
               <div key={passes.indexOf(pass)} id={color[pass.color]} className={styles.passbox}>
-                <h2 style={{ userSelect: "none" }}>{pass.provider}</h2> <Drop userid={userid} pass={pass}/>
+                <h2 style={{ userSelect: "none" }}>{pass.provider}</h2> <Drop userid={userid} pass={pass} />
                 <h3>{pass.account}</h3>
-                <p className={styles.blur}>{pass.password}</p>
+                <p onClick={(e) => visible(e.target, pass.password)} className={styles.blur}>{pass.password}</p>
               </div>
             )
           })
