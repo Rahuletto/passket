e3b66c67180ce3ccd774ed3fd453d795638672d5
Rahul Marban <rahulmarban@gmail.com>
2024-03-23 03:07:37 +0000

    cache enc

--- utils/pbkdf2.ts
+++ utils/pbkdf2.ts
@@ -1,9 +1,19 @@
 // Encrypt-Decrypt
 import CryptoJS from 'crypto-js';
 
+const cache = {};
+
 export function PBKDF2(text: string): string {
-  var key = CryptoJS.PBKDF2(text, process.env.NEXT_PUBLIC_SALT, {
-    keySize: 512 / 32,
-  });
+  var key;
+
+  if (cache[text]) key = cache[text];
+  else {
+    var enc = CryptoJS.PBKDF2(text, process.env.NEXT_PUBLIC_SALT, {
+      keySize: 512 / 32,
+    });
+
+    cache[text] = enc.toString();
+    key = enc;
+  }
   return key.toString();
 }
